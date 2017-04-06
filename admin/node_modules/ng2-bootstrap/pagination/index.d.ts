c309e968971803d0ee727abf923ed75f171737ab
Delushaan <delushaan@outlook.com>
2017-04-06 11:49:36 +0530

    node modules removed

--- admin/node_modules/ng2-bootstrap/pagination/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { PagerComponent } from './pager.component';
-export { PaginationComponent } from './pagination.component';
-export { PaginationModule } from './pagination.module';
--- admin/node_modules/ng2-bootstrap/utils/ng2-bootstrap-config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var browser_1 = require('./facade/browser');
-function isBs3() {
-    return browser_1.window.__theme !== 'bs4';
-}
-exports.isBs3 = isBs3;
