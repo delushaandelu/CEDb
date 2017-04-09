b76558418c68e0c453f38b9253c7a617d8aeda6a
Delushaan <delushaan@outlook.com>
2017-04-09 09:44:03 +0530

    admin user registration completed with out interface data sending to mlab db

---
+++ admin/models/user.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+//User Scheme
+const UserSchema = mongoose.Schema({
+  name:{
+    type: String,
+    required: true
+  },
+  email:{
+    type: String,
+    required: true
+  },
+  regno:{
+    type: String,
+    required: true
+  },
+  telephone:{
+    type: String,
+    required: true
+  },
+  website:{
+    type: String,
+    required: true
+  },
+  youtube_channel:{
+    type: String,
+    required: true
+  },
+  username:{
+    type: String,
+    required: true
+  },
+  password:{
+    type: String,
+    required: true
+  },
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function (id, callback) {
+  User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function (username, callback) {
+  const query = {username: username}
+  User.findOne(query, callback);
+}
+
+module.exports.addUser = function (newUser, callback) {
+  bcrypt.genSalt(10, (err, salt) =>{
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser.save(callback);
+  });
+  });
+}
--- admin/routes/users.js
+++ admin/routes/users.js
@@ -3,14 +3,35 @@
  */
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+//bring the model to the function
+const User = require('../models/user');
 
 //Register
-router.get('/register', (req, res, next) => {
-  res.send('REGISTER');
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    regno: req.body.regno,
+    telephone: req.body.telephone,
+    website: req.body.website,
+    youtube_channel: req.body.youtube_channel,
+    username: req.body.username,
+    password: req.body.password
+  });
+  User.addUser(newUser, (err, user) => {
+    if(err){
+      res.json({success: false, msg:'Failed | Failed to Register User'});
+    } else {
+      res.json({success: true, msg:'Success | User Registered'});
+    }
+  });
 });
 
 //Authenticate
-router.get('/authenticate', (req, res, next) => {
+router.post('/authenticate', (req, res, next) => {
   res.send('Authenticate');
 });
 
@@ -18,11 +39,5 @@ router.get('/authenticate', (req, res, next) => {
 router.get('/profile', (req, res, next) => {
   res.send('profile');
 });
-
-//Validate
-router.get('/validate', (req, res, next) => {
-  res.send('validate');
-});
-
 
 module.exports = router;
