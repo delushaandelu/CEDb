57deedb3c1ee41451738b075a1ab2795630e146b
Delushaan <delushaan@outlook.com>
2017-04-11 19:55:25 +0530

    signup send to db funcetion fully completed

--- admin/angular-src/src/app/components/register/register.component.ts
+++ admin/angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ValidateService} from '../../services/validate.service';
+import {AuthService} from '../../services/auth.service';
+import {FlashMessagesModule, FlashMessagesService} from 'angular2-flash-messages';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -16,9 +20,51 @@ export class RegisterComponent implements OnInit {
   username: String;
   password: String;
 
-  constructor() { }
+  constructor(
+    private validateService: ValidateService,
+    private flahMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    const user ={
+      name : this.name,
+      email : this.email,
+      regno : this.regno,
+      telephone : this.telephone,
+      website : this.website,
+      youtube_channel : this.youtube_channel,
+      logo : this.logo,
+      username : this.username,
+      password : this.password
+    }
+
+    //Required Fileds
+    if(!this.validateService.validateRegister(user)){
+      this.flahMessage.show('Please Fill in all fields', {cssClass:'alert-danger', timeout:3000});
+      return false;
+    }
+
+    //Validate email
+    if(!this.validateService.validateEmail(user.email)){
+      this.flahMessage.show('Please use a valid email', {cssClass:'alert-danger', timeout:3000});
+      return false;
+    }
+
+    //Register User
+    this.authService.registerUser(user).subscribe(data => {
+      if(data.success){
+        this.flahMessage.show('Success | Your are now registered!', {cssClass:'alert-success', timeout:3000});
+        this.router.navigate(['/login']);
+      }else{
+        this.flahMessage.show('Error | Unable to Register!', {cssClass:'alert-danger', timeout:3000});
+        this.router.navigate(['/register']);
+      }
+    });
   }
 
 }
