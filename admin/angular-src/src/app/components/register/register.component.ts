146170ce08c9893bbf1f4957935caeb64f29a77d
Delushaan <delushaan@outlook.com>
2017-04-11 10:25:20 +0530

    admin ui attached login nav bar and signup

--- admin/angular-src/src/app/components/register/register.component.ts
+++ admin/angular-src/src/app/components/register/register.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  name: String;
+  email: String;
+  regno: String;
+  telephone: String;
+  website: String;
+  youtube_channel: String;
+  logo: String;
+  username: String;
+  password: String;
 
   constructor() { }
 
