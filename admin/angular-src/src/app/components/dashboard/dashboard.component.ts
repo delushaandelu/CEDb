0c413dc62988c4bf5a7fbd5cc9b9d4264a3135fd
Delushaan <delushaan@outlook.com>
2017-04-12 19:17:51 +0530

    view modifed

--- admin/angular-src/src/app/components/dashboard/dashboard.component.ts
+++ admin/angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  user:Object;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.authService.getProfile().subscribe(dashboard =>{
+        this.user = dashboard.user;
+      },
+      err => {
+        console.log(err);
+        return false;
+      });
   }
-
 }
