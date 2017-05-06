fd7144874da7447999af94b7cc3854d930417baa
Delushaan <delushaan@outlook.com>
2017-05-06 12:12:28 +0530

    admin part

---
+++ admin/angular-src/src/app/components/addmovie/addmovie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addmovie',
+  templateUrl: './addmovie.component.html',
+  styleUrls: ['./addmovie.component.css']
+})
+export class AddmovieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
